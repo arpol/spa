ed03c729fe6d1704932e19d673cc22704912e493
Lari Rasku <raskug@lavabit.com>
2013-01-31 01:02:59 +0200

    Turns out we're sorting beyond US-ASCII after all
    Also, switched back to sorted() for the moment.

--- radixsort.py
+++ radixsort.py
@@ -1,5 +1,4 @@
 import itertools
-from quicksort import QuickSort
 
 def msd(iterable):
     stack = [(iterable, 0)]
@@ -9,14 +8,14 @@ def msd(iterable):
             for string in iterable:
                 yield string
         else:
-            buckets = [list() for x in range(128)]
+            buckets = [list() for x in range(256)]
             for string in iterable:
                 key = ord(string[depth] if depth < len(string) else '\0')
                 buckets[key].append(string)
             for bucket in reversed(buckets):
                 if bucket:
-                    if len(bucket) < 12:
-                        stack.append((QuickSort(bucket, depth+1), -1))
+                    if len(bucket) < 16:
+                        stack.append((sorted(bucket), -1))
                     else:
                         stack.append((bucket, depth+1))
             stack[-1] = (buckets[0], -1)
@@ -27,7 +26,7 @@ def lsd(iterable):
     for depth in reversed(range(len(array[bound]))):
         while bound > 0 and len(array[bound-1]) > depth:
             bound -= 1
-        buckets = [list() for x in range(128)]
+        buckets = [list() for x in range(256)]
         for string in array[bound:]:
             key = ord(string[depth])
             buckets[key].append(string)
