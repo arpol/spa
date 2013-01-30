ea4c31fa80de48ee2fda7895f1c7d92811ccddaf
Lari Rasku <raskug@lavabit.com>
2013-01-30 02:55:43 +0200

    Fixed the bounds a little
    I doubt we'll be sorting anything besides US-ASCII, and the theoretically
    optimal bound for falling back to a comparison sort is the square root of
    the size of the alphabet; this should probably be verified in practice,
    though.

--- radixsort.py
+++ radixsort.py
@@ -8,13 +8,13 @@ def msd(iterable):
             for string in iterable:
                 yield string
         else:
-            buckets = [list() for x in range(256)]
+            buckets = [list() for x in range(128)]
             for string in iterable:
                 key = ord(string[depth] if depth < len(string) else '\0')
                 buckets[key].append(string)
             for bucket in reversed(buckets):
                 if bucket:
-                    if len(bucket) < 256:
+                    if len(bucket) < 12:
                         stack.append((QuickSort(bucket, depth+1), -1))
                     else:
                         stack.append((bucket, depth+1))
