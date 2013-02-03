734fcdebbe2af46901a6210eba482dfcc3acf63f
Arturs Polis <arturs.polis@gmail.com>
2013-02-03 14:46:45 +0200

    Fixed minor bugs in quicksort

---
+++ listify.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+import sys
+import re
+
+def listify(filepath) :
+    """
+    Converts the input text into a line separated list of words
+    
+    Input: 
+        filepath - path to the dataset filepath
+    
+    Output: 
+        dataset converted to one word by line format
+    """
+    output = []
+    f = open(filepath)
+    for line in f :
+        output += re.findall(r"[\w']+", line)
+    for S in output :
+        print S
+    #print output
+    
+if __name__ == "__main__" :
+    listify(sys.argv[1])
