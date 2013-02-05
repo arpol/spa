e9bc2bd0eaf2b191abbdc9fd619df43f5a11de47
Lari Rasku <raskug@lavabit.com>
2013-02-05 22:51:14 +0200

    Fixed some bugs with radix sort

--- radixsort.py
+++ radixsort.py
@@ -1,7 +1,51 @@
-import itertools
-from quicksort import QuickSort
+# -*- coding: utf-8 -*-
+
+import insertionsort
+import quicksort
 
 def msd(iterable, depth=0):
+    stack = [(iterable, depth)]
+    while stack:
+        iterable, depth = stack.pop()
+        if depth < 0:
+            for string in iterable:
+                yield string
+        else:
+            buckets = [list() for x in range(256)]
+            for string in iterable:
+                key = ord(string[depth]) if depth < len(string) else 0
+                buckets[key].append(string)
+            for bucket in reversed(buckets):
+                if bucket:
+                    if len(bucket) < 256:
+                        stack.append((quicksort.multikey(bucket, depth+1), -1))
+                    else:
+                        stack.append((bucket, depth+1))
+            if buckets[0]:
+                stack[-1] = (buckets[0], -1)
+
+def insertion(iterable, depth=0):
+    stack = [(iterable, depth)]
+    while stack:
+        iterable, depth = stack.pop()
+        if depth < 0:
+            for string in iterable:
+                yield string
+        else:
+            buckets = [list() for x in range(256)]
+            for string in iterable:
+                key = ord(string[depth] if depth < len(string) else '\0')
+                buckets[key].append(string)
+            for bucket in reversed(buckets):
+                if bucket:
+                    if len(bucket) < 16:
+                        stack.append((insertionsort.multikey(bucket, depth+1), -1))
+                    else:
+                        stack.append((bucket, depth+1))
+            if buckets[0]:
+                stack[-1] = (buckets[0], -1)
+
+def quickinsert(iterable, depth=0):
     stack = [(iterable, depth)]
     while stack:
         iterable, depth = stack.pop()
@@ -16,7 +60,8 @@ def msd(iterable, depth=0):
             for bucket in reversed(buckets):
                 if bucket:
                     if len(bucket) < 256:
-                        stack.append((QuickSort(bucket, depth+1), -1))
+                        stack.append((quicksort.insertion(bucket, depth+1), -1))
                     else:
                         stack.append((bucket, depth+1))
-            stack[-1] = (buckets[0], -1)
+            if buckets[0]:
+                stack[-1] = (buckets[0], -1)
