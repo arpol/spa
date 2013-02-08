47484cb2b6fa0e937c5763ba9586bd4ad89e2e66
Lari Rasku <raskug@lavabit.com>
2013-02-08 19:45:25 +0200

    Some seamless improvements

--- radixsort.py
+++ radixsort.py
@@ -5,20 +5,19 @@ import insertionsort
 def msd(iterable, depth=0):
     stack = [(iterable, depth)]
     while stack:
-        iterable, depth = stack.pop()
+        bucket, depth = stack.pop()
         if depth < 0:
-            for string in iterable:
+            for string in bucket:
                 yield string
         else:
             buckets = [list() for x in range(256)]
-            for string in iterable:
-                key = ord(string[depth] if depth < len(string) else '\0')
-                buckets[key].append(string)
+            for string in bucket:
+                if len(string) > depth:
+                    buckets[ord(string[depth])].append(string)
+                else:
+                    yield string
             for bucket in reversed(buckets):
-                if bucket:
-                    if len(bucket) < 16:
-                        stack.append((insertionsort.multikey(bucket, depth+1), -1))
-                    else:
-                        stack.append((bucket, depth+1))
-            if buckets[0]:
-                stack[-1] = (buckets[0], -1)
+                if len(bucket) < 256:
+                    stack.append((quicksort.QuickSort(bucket, depth+1), -1))
+                else:
+                    stack.append((bucket, depth+1))
